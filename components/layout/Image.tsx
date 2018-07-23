c90c3fd041eb7c08de5fffbaba3a5ee381c89ee7
Javier Carrillo Milla <stringparser@gmail.com>
2018-07-23 17:58:40 +0200

    alpha fixes, Background image src

--- components/layout/Image.tsx
+++ components/layout/Image.tsx
@@ -9,14 +9,16 @@ type ImageProps = {
   height?: number;
 };
 
-const Image = styled<ImageProps, 'img'>('img')
-  .attrs({
-    src: ({ src }: ImageProps) => `${BACKEND_URL}${src}`,
-  })
-  `
-    max-width: ${({ dim, width }) => width || dim || 150}px;
-    max-height: ${({ dim, height }) => height || dim || 150}px;
-  `
-;
+const Image = styled<ImageProps, 'img'>('img')`
+  max-width: ${({ dim, width }) => width || dim || 150}px;
+  max-height: ${({ dim, height }) => height || dim || 150}px;
+`;
 
-export default Image;
+const _Image: React.SFC<ImageProps> = ({
+  src,
+  ...props
+}) => (
+  <Image src={`${BACKEND_URL}${src}`} {...props} />
+);
+
+export default _Image;
