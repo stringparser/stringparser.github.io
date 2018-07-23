74f92154b64a5e2b7e38eb2a5602bd1110d95f78
Javier Carrillo Milla <stringparser@gmail.com>
2018-07-23 18:05:47 +0200

    Deploy Next.js to gh-pages

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6e2402f301ed2078639e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "af7e186976250c50cdaf"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
