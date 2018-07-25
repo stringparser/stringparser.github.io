b8f35e2ae0c09f690f2551c470508d32715e4684
Javier Carrillo Milla <stringparser@gmail.com>
2018-07-25 17:22:21 +0200

    feature(analytics): adds SPA event tracking

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,4 +1,18 @@
+
+import Router from 'next/router';
 import { BACKEND_URL } from '../config/client';
+
+const oldOnRouteChange = Router.onRouteChangeComplete;
+
+Router.onRouteChangeComplete = (url: string) => {
+  if (typeof oldOnRouteChange === 'function') {
+    oldOnRouteChange(url);
+  }
+
+  if (window.analytics) {
+    window.analytics.page();
+  }
+};
 
 export const AnalyticsHeadScript = () => (
   <script
