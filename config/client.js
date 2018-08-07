37fc2e00387c646f400e2d91014d487ced2f5c2b
Javier Carrillo Milla <stringparser@gmail.com>
2018-08-07 12:23:27 +0200

    chore(config): update production BACKEND_URL

--- config/client.js
+++ config/client.js
@@ -6,7 +6,9 @@ exports = module.exports = {
   EMAIL: 'stringparser@gmail.com',
   GITHUB_URL: 'https://github.com/stringparser',
   TWITTER_URL: 'https://twitter.com/stringparser',
-  BACKEND_URL: process.env.NODE_ENV === 'production' ? 'https://stringparser.github.io' : '',
+  BACKEND_URL: process.env.NODE_ENV === 'production'
+    ? 'https://www.jacami.co'
+    : '',
   LINKEDIN_URL: 'https://www.linkedin.com/in/stringparser',
   META_KEYWORDS: ['software', 'engineer'].concat(...technologies).join(', ').toLowerCase(),
 };
