e6f373a1b78862aa7a4baaf51ac2ff5d01e14c83
Javier Carrillo Milla <stringparser@gmail.com>
2022-03-02 13:46:29 +0100

    shorter wording for contact section

--- components/section/ContactSection.tsx
+++ components/section/ContactSection.tsx
@@ -23,7 +23,7 @@ const ContactSection: React.FC<Props> = (props) => (
 
         <Text>
           You can always send me an <EmailMeLink />{' '}
-          or find me as @stringparser in{' '}
+          or find me in{' '}
             <a href={GITHUB_URL}>github</a>,{' '}
             <a href={TWITTER_URL}>twitter</a> or{' '}
             <a href={LINKEDIN_URL}>linkedin</a>
