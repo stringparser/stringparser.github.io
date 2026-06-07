65f9cc43a84c5403a488bb2b2ddf357ce28ebc9c
Javier Carrillo Milla <stringparser@gmail.com>
2026-06-07 16:54:43 +0200

    new site version

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import type { Config } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-
-const config: Config = [
-  ...nextCoreWebVitals,
-];
-
-export default config;
--- src/components/BlinkingCursor.tsx
+++
@@ -1,8 +0,0 @@
-export function BlinkingCursor() {
-  return (
-    <span
-      aria-hidden="true"
-      className="ml-1.5 inline-block h-[0.85em] w-[0.18em] translate-y-[0.08em] animate-blink bg-white/75"
-    />
-  );
-}
